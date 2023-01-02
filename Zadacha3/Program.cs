bdab33720f5c42c8eabbffb0ecc4a38b1397b5c0
AndreyMosiakov <mosiakoff@yandex.ru>
2023-01-02 20:37:27 +0300

    добавили задачу3

---
+++ Zadacha3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа принимает на вход одно число (N), на выходе показывает все целые числа от -N до N
+Console.Clear();
+Console.WriteLine("Введите число");
+int numberN = int.Parse(Console.ReadLine()!);
+int numberC = -numberN;
+while(numberC <= numberN)
+{
+    Console.Write(numberC+ ",");
+    numberC++; 
+}
