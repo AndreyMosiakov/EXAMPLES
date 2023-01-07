5bccfe86fb02452aefa166bcfb113c5e08868796
AndreyMosiakov <mosiakoff@yandex.ru>
2023-01-07 22:20:40 +0300

    add task10

---
+++ Smnr2_task10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу , которая выводит случайное число из отрезка[10, 99] и показывает наибольшую цифру числа.
+Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+}
