7f8bf7d397f73a6f766fc26402c3b4b8dfba90e8
AndreyMosiakov <mosiakoff@yandex.ru>
2023-01-07 23:02:01 +0300

    finish task10

--- Smnr2_task10/Program.cs
+++ Smnr2_task10/Program.cs
@@ -4,4 +4,21 @@ Console.Clear();
 int GetNumberFromRange(int start, int end)
 {
     int number = new Random().Next(start, end + 1);
+    return number;
 }
+int GetMaxDigit(int number)
+{
+    int max = number % 10;
+    int digit2 = number / 10;
+    
+    if (digit2 > max)
+    {
+      max = digit2; 
+    }
+    return max;
+}
+
+int randomNumber = GetNumberFromRange(10, 99);
+int maxDigit = GetMaxDigit(randomNumber);
+Console.WriteLine(randomNumber);
+Console.WriteLine(maxDigit);
