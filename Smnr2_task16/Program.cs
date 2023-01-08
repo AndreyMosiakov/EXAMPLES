e7b2150c83c5ab16746a6339ea4ce4860005f067
AndreyMosiakov <mosiakoff@yandex.ru>
2023-01-08 15:21:19 +0300

    add task16

---
+++ Smnr2_task16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу , которая принимает на вход два числа и проверяет является ли одно число квадратом другого
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 == num2*num2)
+{
+    Console.WriteLine($"первое число {num1} является квадратом второго {num2}");
+}
+else if (num2 == num1*num1)
+{
+    Console.WriteLine($"второе число {num2} является квадратом первого {num1}");
+}
+else
+{
+    Console.WriteLine("введеные числа не отвечают условиям задачи");
+}
