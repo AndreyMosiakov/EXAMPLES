fe1467b8ead2af480162c39d98fada1ec8f33bd8
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-11 20:59:07 +0300

    add task18

---
+++ Smnr3_task18/Program.cs
@@ -0,0 +1,48 @@
+﻿// Программа показывающая по заданному номеру четверти диапазон возможных координат точек.
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+string getRangeFromQuarter(int quarter)
+{
+    string result = $"Диапазон значений в {quarter} четверти";
+    switch (quarter) // конструкция if else
+    {
+        case 1:
+            {
+                result += " x>0 и y>0";// + перед равно добавит текст к шаблону из метода
+                break;
+            }
+        case 2:
+            {
+                result += " x<0 и y>0";
+                break;
+            }
+        case 3:
+            {
+                result += " x<0 и y<0";
+                break;
+            }
+        case 4:
+            {
+                result += " x>0 и y<0";
+                break;
+            }
+        default:
+            {
+                result = $" Такой четверти не существует";
+                break;
+            }
+    }
+    return result;
+
+}
+
+int quarter = getUserValue("Введите номер четверти");
+string RangeDescription = getRangeFromQuarter(quarter);
+Console.WriteLine(RangeDescription);
