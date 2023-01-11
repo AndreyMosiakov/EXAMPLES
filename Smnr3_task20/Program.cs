3abadcb85a5864f56057fa13bdcd213144c2484c
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-11 21:28:04 +0300

    add task20

---
+++ Smnr3_task20/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа принимает на вход две точки и находит расстояние между ними в 2D пространстве
+
+Console.Clear();
+
+int getUserValue(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+double getDistanceCoordinate(int userAx, int userAy, int userBx, int userBy)
+{
+    double result = Math.Sqrt(Math.Pow((userAx-userBx))+Math.Pow((userAy-userBy)));// формула вычисления
+    return result;
+}
+
+int userAx = getUserValue ("Введите X");
+int userAy = getUserValue ("Введите Y");
+
+int userBx = getUserValue ("Введите X");
+int userBy = getUserValue ("Введите Y");
