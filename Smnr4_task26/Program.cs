be8fa706cd0900549f79e2dda50d87df2bfa0656
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-16 21:13:49 +0300

    add task26

---
+++ Smnr4_task26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26 . Прграмма принимает на вход число и выдаёт кол-во цифр в числе.
+
+Console.Clear();
+
+int getUserDate(string message)  // `функция получения информации от пользователя
+{
+    Console.WriteLine(message);
+    int userDate = int.Parse(Console.ReadLine()!);
+    return userDate;
+}
+int getNumberOfDigit(int number)
+{
+    int numberOfDigit = 0;
+    while (number > 0)
+    {
+        number = number / 10;
+        numberOfDigit++;
+    }
+    return numberOfDigit;
+}
+int number = getUserDate("Введите число ");
+int numberOfDigit = getNumberOfDigit(number);
+Console.WriteLine($"В числе {number} присутствует {numberOfDigit} цифр(ы)");
