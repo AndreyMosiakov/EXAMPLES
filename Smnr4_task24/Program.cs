5046b427811eb61d38cb93b203625c9db6e0f37c
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-16 20:40:19 +0300

    add task24

---
+++ Smnr4_task24/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 24,Программа принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+Console.Clear();
+
+int getUserDate(string message)  // `функция получения информации от пользователя
+{
+    Console.WriteLine(message);
+    int userDate = int.Parse(Console.ReadLine()!);
+    return userDate;
+}
+
+int getSumOfRange(int start, int end)
+{
+    int sum = 0;
+    for (int i = start; i <= end; i++)
+    {
+        sum += i; // sum=sum+1
+    }
+    return sum;
+}
+
+int end = getUserDate("Введите число "); // запуск функции
+int sum = getSumOfRange(1, end);
+Console.WriteLine($"Сумма чисел от 1 до {end} = {sum}");
