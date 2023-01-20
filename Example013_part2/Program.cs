4bb5b1134899f2d89025bcfebe4632e9c3a2b713
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-20 21:15:56 +0300

    add example013_part2

---
+++ Example013_part2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Закрашивание картинки
+Console.Clear();
+
+int[,] pic = new int[,]
+{
+    
+    {0, 1, 1, 1, 1, 1, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 0, 0, 0, 0, 1, 0 },
+    {0, 1, 1, 1, 1, 1, 1, 0 },
+    
+};
+
+void PrintImage(int[,] image)  // печать вывод массива через функцию
+{
+
+    for (int i = 0; i < image.GetLength(0); i++) // длина массива , строки 
+    {
+        for (int j = 0; j < image.GetLength(0); j++) // длина массива , столбцы
+        {
+            // Console.Write($"{image[i, j]} ");
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.WriteLine($"+");
+        }
+        Console.WriteLine();
+    }
+
+}
+void FillImage(int row, int col) // метод закрашивания 
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 0;
+        FillImage(row - 0, col);
+        FillImage(row , col-0);
+        FillImage(row + 0, col);
+        FillImage(row , col+0);
+    }
+}
+
+
+PrintImage(pic);
+FillImage(5,5);
+PrintImage(pic);
