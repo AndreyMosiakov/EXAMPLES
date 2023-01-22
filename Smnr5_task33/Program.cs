65d62480ee3d5e5593938c8769503b70f7aaa7e3
AndreyMosiakov <mosiakoff@yandex.ru>
2023-01-22 16:17:04 +0300

    add task33

---
+++ Smnr5_task33/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 33. Введите массив , напишите программу которая определяет присутствует ли заданное число в массиве.
+Console.Clear();
+
+int[] GetArrayRandom(int lenght, int strt, int end) // функция ввода рандомного массива
+{
+    int[] arr = new int[lenght];
+    for (int i = 0; i < lenght; i++)
+    {
+        arr[i] = new Random().Next(strt, end + 1); // ограничил диапазон случайных чисел
+    }
+    return arr;
+}
+
+int GetFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine());
+    return userInt;
+}
+
+void showArray(int[] array)  // функция для вывода массива , что бы после последнего символа не было запятой
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) // !=  -оператор не равно
+        {
+            Console.Write($"{array[i]},");
+        }
+        else
+        {
+            Console.WriteLine($"{array[i]}]");
+        }
+    }
+}
+int searchNumber(int[] array, int number)
+{
+    int result = -1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
+int number = GetFromUser("введите число");
+int[] array = GetArrayRandom(15, -10, 10);
+showArray(array);
+int findnumber = searchNumber(array, number);
+if(findnumber==-1)
+{
+    Console.WriteLine("Число не найдено");
+}
+else
+{
+    Console.WriteLine($"Число{number} есть в списке под индексом {findnumber}");
+}
