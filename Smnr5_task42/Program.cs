a1cb95588b8f7e8cb3a0d8f4e690ccb88c29567b
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-01-25 20:29:43 +0300

    add task42

---
+++ Smnr5_task42/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 42. Напишите программу , которая переводит десятичное число в двоичное.
+// 45 ->101101
+// 3->11
+// 2->10
+
+Console.Clear();
+
+int GetFromUser(string message)
+{
+    Console.WriteLine(message);
+    int userInt = int.Parse(Console.ReadLine()!);
+    return userInt;
+}
+string GetBinaryNumber(int number)
+{
+    string result = string.Empty;
+    while (number > 0)
+    {
+        result = number % 2 + result; // деление на два с остатком (если есть остаток то это 1 нет это два)
+        number = number / 2;
+    }
+    return result;
+
+}
+
+int number = GetFromUser("введите число");
+string binary = GetBinaryNumber(number);
+Console.WriteLine($" Число {number} в двоичной системе = {binary} ");
