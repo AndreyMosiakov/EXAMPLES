3515c05b2c9a0e5b229aef7a1f2b1a0cfec7ecd9
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-02-06 20:52:16 +0300

    add task67

---
+++ Smnr9_task67/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 67 : Напишите программу , которая будет принимать на вход число и возвращать сумму его чисел.
+
+Console.Clear();
+
+int GetDataFromUser(string messege)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(messege);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+int getSumOfDigit(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return number % 10 + getSumOfDigit(number / 10);// Рекурсия
+}
+
+int userNumber = GetDataFromUser("введите число");
+int result = getSumOfDigit(userNumber);
+Console.WriteLine(result);
