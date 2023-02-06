7b5ad97d5006b099354deaf7fd32578bc643c4c4
andrey_mosiakoff.ru <mosiakoff@yandex.ru>
2023-02-06 20:24:29 +0300

    add task65

---
+++ Smnr9_task65/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 65 Задайте значения M и N . Напишите программу , которая выдаёт все натуральные числа от M до N.
+Console.Clear();
+
+int GetDataFromUser(string messege)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(messege);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int userStartNumber = GetDataFromUser("введите значение M");
+int userFinishNumber = GetDataFromUser("введите значение N");
+string rage = getStrRange(userStartNumber, userFinishNumber);
+Console.WriteLine(rage);
+string getStrRange ( int start, int finish)
+{
+   if(start==finish) 
+   {
+    return start +" ";
+   }
+   return start +","+ getStrRange(start+1, finish);
+
+}
